1061176d86c6fdfbf9091ecaf621c240e0aa0e88
Bror Brurberg <bror@hey.com>
2022-11-08 08:40:37 +0100

    Initial Commit

---
+++ Pooptracker/PooptrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PooptrackerApp.swift
+//  Pooptracker
+//
+//  Created by Bror2 on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PooptrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
